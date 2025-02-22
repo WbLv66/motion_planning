186478bcab41a31cb643306f68d7f235433d4dbd
lv wenbo <wb.lv@qq.com>
2025-02-22 16:58:53 +0800

    Add comment

--- src/path_planning/include/path_planning/bfs.h
+++ src/path_planning/include/path_planning/bfs.h
@@ -8,13 +8,33 @@
 #include <queue>
 
 #include "planer.h"
+/**
+ * @brief Bfs class inherits Planer class
+ */
 class Bfs : public Planer<std::queue<Node>> {
 public:
     Bfs() = delete;
+    /**
+     * @brief Constructor for Bfs class
+     * @param grid_map: grid mao
+     */
     Bfs(const std::vector<std::vector<int>> &grid_map);
+    /**
+     * @brief Default destructor override for Bfs class
+     */
     ~Bfs() override = default;
+    /**
+     *@brief Override function that initialize the open_list_
+     *@param start: start node
+     */
     void Initialization(const Node &start) override;
+    /**
+     *@brief Override function that visit the node
+     */
     void VisitNode() override;
+    /**
+     *@brief Override function that expand the node
+     */
     void ExpandNode() override;
 };
 #endif
--- src/path_planning/include/path_planning/planer.h
+++ src/path_planning/include/path_planning/planer.h
@@ -12,8 +12,6 @@
 #include <unordered_set>
 #include <vector>
 
-template <class T>
-class Planer;
 /**
  * @brief Node class for planning
  * @param row_: row index of the node
@@ -24,18 +22,66 @@ class Planer;
  */
 class Node {
 public:
+    /**
+     * @brief Default constructor for Node class
+     */
     Node() = default;
+    /**
+     * @brief Default destructor for Node class
+     */
     ~Node() = default;
+    /**
+     * @brief Constructor for Node class
+     * @param row: row index of the node
+     * @param col: column index of the node
+     * @param cost_from_start: cost from start to this node, the default value
+     * is infinite
+     * @param parent: parent node, the default value is nullptr
+     */
     Node(int row, int col,
          double cost_from_start = std::numeric_limits<double>::max(),
          const Node *parent = nullptr);
+    /**
+     * @brief Copy constructor for Node class
+     * @param other: the node to be copied
+     */
     Node(const Node &other) = default;
+    /**
+     * @brief Copy assignment operator for Node class
+     * @param other: the node to be copied
+     */
     Node &operator=(const Node &other) = default;
+    /**
+     * @brief Move constructor for Node class
+     * @param other: the node to be moved
+     */
+    Node(Node &&other) = default;
+    /**
+     * @brief Move assignment operator for Node class
+     * @param other: the node to be moved
+     */
     Node &operator=(Node &&other) = default;
+    /**
+     * @brief Overload operator == for Node class
+     * @param other: the node to be compared
+     * @return bool: whether the two nodes are equal
+     */
     bool operator==(const Node &other) const;
+    /**
+     * @brief Overload operator + for Node class
+     * @param other: the node to be added
+     * @return Node: the sum of two nodes
+     */
     Node operator+(const Node &other) const;
-
+    /**
+     * @brief Adjust whether the node is in the map
+     * @return bool: whether the node is in the map
+     */
     bool InMap() const;
+    /**
+     * @brief Adjust whether the node is an obstacle
+     * @return bool: whether the node is an obstacle
+     */
     bool IsObstacle() const;
 
     int row_{};
@@ -46,6 +92,12 @@ public:
 };
 
 namespace std {
+/**
+ * @brief Hash function for Node class
+ * @param node: the node to be hashed
+ * @return size_t: the hash value of the node
+ * @details  hash value is determined by row_ and col_
+ */
 template <>
 struct hash<Node> {
     size_t operator()(const Node &node) const {
@@ -53,32 +105,103 @@ struct hash<Node> {
     }
 };
 }  // namespace std
-
+/**
+ * @brief Template base class Planer
+ * @param T: the type of open_list_
+ * @param grid_map(): grid map, can be used as static variable
+ * @param grid_row_(): rows number, can be used as static variable
+ * @param grid_col_(): columns number, can be used as static variable
+ * @param open_list_: contains nodes which will be visited
+ * @param close_list_: contains nodes which have been visited
+ * @param grid_path_: the grid path obtained
+ * @param current_node_: current node
+ * @param next_node_: neighbor node
+ * @param four_directions_(): expand to four directions, can be used as static
+ * variable
+ * @param eight_directions_(): expand to eight directions, can be used as static
+ * variable
+ */
 template <class T>
 class Planer {
 public:
     Planer() = delete;
+    /**
+     * @brief Constructor for Planer class
+     * @param grid_map: grid map
+     */
     Planer(const std::vector<std::vector<int>> &grid_map);
+    /**
+     * @brief Virtual default destructor for Planer class
+     */
     virtual ~Planer() = default;
+    /**
+     * @brief Plan the path from start to goal
+     * @param start: start node
+     * @param goal: goal node
+     * @return std::stack<Node>: the grid path
+     * @details designd using template method pattern
+     */
     std::stack<Node> &Plan(const Node &start, const Node &goal);
-
+    /**
+     *@brief Grid map
+     *@return std::vector<std::vector<int>>&: reference to static variable
+     *@details static function which creats static variable
+     */
     static std::vector<std::vector<int>> &grid_map_();
+    /**
+     *@brief Rows number
+     *@return int&: reference to static variable
+     *@details static function which creats static variable
+     */
     static int &grid_row_();
+    /**
+     *@brief Columns number
+     *@return int&: reference to static variable
+     *@details static function which creats static variable
+     */
     static int &grid_col_();
 
 protected:
-    virtual void Initialization(const Node &start) {};
-    virtual void VisitNode() {};
+    /**
+     *@brief Virtual function that initialize the open_list_
+     *@param start: start node
+     */
+    virtual void Initialization(const Node &start){};
+    /**
+     *@brief Virtual function that visit the node
+     */
+    virtual void VisitNode(){};
+    /**
+     *@brief Adjust whether get to goal
+     *@param goal: goal node
+     *@return bool: whether get to goal
+     */
     bool GetToGoal(const Node &goal);
+    /**
+     *@brief Backtrack to obtain path
+     */
     void PathBacktrack();
-    virtual void ExpandNode() {};
+    /**
+     *@brief Virtual function that expand the node
+     */
+    virtual void ExpandNode(){};
 
     T open_list_;
     std::unordered_set<Node> close_list_;
     std::stack<Node> grid_path_{};
     Node current_node_{};
     Node next_node_{};
+    /**
+     *@brief Expand to four directions
+     *@return const std::vector<Node> &: reference to static variable
+     *@details static function which creats static variable
+     */
     static const std::vector<Node> &four_directions_();
+    /**
+     *@brief Expand to eight directions
+     *@return const std::vector<Node> &: reference to static variable
+     *@details static function which creats static variable
+     */
     static const std::vector<Node> &eight_directions_();
 };
 
